e0e723674d189a7dd5061a412ac071304d2e7faf
Vladyslav-Kruchok <vladyslavw@gmail.com>
2022-08-07 19:51:04 +0000

    Deploying to gh-pages from @ Vladyslav-Kruchok/goit-react-hw-05-movies@3178e03dec4ce83fd8af1d9596bd8a6988ee6bb6 🚀

---
+++ static/js/NotFound.85cf294b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[187],{9256:function(e,t,c){c.r(t);var i=c(184);t.default=function(){return(0,i.jsx)("h1",{children:"NOT FOUND"})}}}]);
+//# sourceMappingURL=NotFound.85cf294b.chunk.js.map
